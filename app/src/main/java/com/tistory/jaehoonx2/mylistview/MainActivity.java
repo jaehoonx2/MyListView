af7497fa8d7169fe0316e2e789ae8dac9e224d7e
jaehoonx2 <21300698@handong.edu>
2018-07-19 02:42:01 +0900

    Initial commit

---
+++ app/src/main/java/com/tistory/jaehoonx2/mylistview/MainActivity.java
@@ -0,0 +1,103 @@
+package com.tistory.jaehoonx2.mylistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    SingerAdapter adapter;
+    EditText editText;
+    EditText editText2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        adapter = new SingerAdapter();
+        adapter.addItem(new SingerItem("소녀시대", "010-6577-5125", R.drawable.singer));
+        adapter.addItem(new SingerItem("AOA", "010-6577-5125", R.drawable.singer2));
+        adapter.addItem(new SingerItem("걸스데이", "010-6577-5125", R.drawable.singer3));
+        adapter.addItem(new SingerItem("소나무", "010-6577-5125", R.drawable.singer4));
+        adapter.addItem(new SingerItem("러블리즈", "010-6577-5125", R.drawable.singer5));
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SingerItem item = (SingerItem) adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), "선택 : " + item.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+                String mobile = editText2.getText().toString();
+
+                adapter.addItem(new SingerItem(name, mobile, R.drawable.singer));
+                adapter.notifyDataSetChanged(); // listview 갱신
+
+            }
+        });
+    }
+
+    class SingerAdapter extends BaseAdapter {
+        ArrayList<SingerItem> items = new ArrayList<SingerItem>();
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            SingerItemView view = null;
+
+            if(convertView == null) {
+                view = new SingerItemView(getApplicationContext());
+            } else {
+                view = (SingerItemView) convertView;
+            }
+
+            SingerItem item = items.get(position);
+            view.setName(item.getName());
+            view.setMobile(item.getMobile());
+            view.setImage(item.getResId());
+            return view;
+        }
+
+        public void addItem(SingerItem item){
+            items.add(item);
+
+        }
+    }
+}
